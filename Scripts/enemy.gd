9ff31953f1dca68862a5676820dee37a1992925d
Funfire <humzanomi@gmail.com>
2023-02-08 16:33:33 -0500

    h

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-
+onready var main = load("res://Scripts/Main.gd")
+onready var  mm = main.get("moneyCount")
 onready var enemy = load("res://Scenes/enemy.tscn")
 var nums = [140, 246]
 
 func _physics_process(delta):
 	$".".position.x -= 5
-
 func SpawnEnemy():
 	var e = enemy.instance()
 	get_parent().add_child(e)
@@ -16,6 +16,7 @@ func _on_Area2D_area_entered(area:Area2D):
 	if area.name == "SliceZone":
 		queue_free()
 		SpawnEnemy()
+		mm += 5
 	else:
 		queue_free()
 		SpawnEnemy()
