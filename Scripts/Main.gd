96cc3a7ea95dd1b391067882e4a4d33fe0d3429a
Funfire <humzanomi@gmail.com>
2023-02-08 20:41:42 -0500

    new

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,11 +1,26 @@
 extends Node2D
 
-var moneyCount = 0
 
- 
+var moneyCount = 0
+onready var s = get_node("enemy").get("Speed")
+var canBuy = true
+
 func _physics_process(delta):
 	$Money.text = str(moneyCount) + "$"
+	if moneyCount <= 0:
+		canBuy = false
+	elif moneyCount > 0:
+		canBuy = true
 
 func _on_Timer_timeout():
 	moneyCount += 1
 	$Money/Timer.start()
+
+func _on_SliceZone_area_entered(area:Area2D):
+	if area.name == "EnemyHitbox":
+		moneyCount += 5
+
+func _on_Button_pressed():
+	if canBuy == true and moneyCount >= 5:
+		moneyCount -= 5
+		s += 5
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
-onready var main = load("res://Scripts/Main.gd")
-onready var  mm = main.get("moneyCount")
 onready var enemy = load("res://Scenes/enemy.tscn")
 var nums = [140, 246]
+var Speed = 5
 
 func _physics_process(delta):
-	$".".position.x -= 5
+	$".".position.x -= Speed
+	print(Speed)
+	if Speed == 10:
+		print("Speed up")
+
+
 func SpawnEnemy():
 	var e = enemy.instance()
 	get_parent().add_child(e)
@@ -16,10 +20,11 @@ func _on_Area2D_area_entered(area:Area2D):
 	if area.name == "SliceZone":
 		queue_free()
 		SpawnEnemy()
-		mm += 5
 	else:
 		queue_free()
 		SpawnEnemy()
+
+
 
 
 
